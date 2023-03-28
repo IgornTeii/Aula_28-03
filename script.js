d79014d7b1571fca72c5e080d9b349513dde40df
Igor <igornteixeira48@gmail.com>
2023-03-28 11:51:26 -0300

    AULA DIA 28/03

---
+++ script.js
@@ -0,0 +1,68 @@
+// Sustituir o texto
+
+//let texto = 'Estão chegando as provas'
+//console.log(texto.replace('Provas','Avaliações'))
+
+// Fixar o valor
+
+//let num = 223.45555
+//console.log(num.toFixed(2)) 
+
+// Alert box 
+
+//alert('Olá meu amigo(a)')
+
+// caixa de confirmacao
+
+//let test = confirm('Você é aluno?')
+//console.log(test)
+
+//Caixa de prompt 
+
+//let text1 = prompt('Digite seu nome')
+//console.log('O nome dele é:',text1)
+//document.write('O meu nome é ', text1)
+
+//if
+//if(20 >= 20){
+   // document.write('20 é maior ')
+//}
+
+//if else
+//if(25 > 15){
+    //document.write('Numero maior<br>')
+//}else{
+   // document.write('Numero menor')
+//}
+
+//if else declarando variavel 
+//let numero = 10
+
+//if(numero > 10){
+    //document.write('O numero é maior')
+//}else{
+    //document.write('O numero é menor')
+//}
+
+//let idade = 18 
+//let idade = prompt('Digite sua idade')
+
+//if(idade <= 12){
+    //document.write('É UMA CRIANÇA')
+
+//}else if(idade <= 17){
+    //document.write('É UM ADOLESCENTE')
+
+//}else if(idade <= 30){
+    //document.write('É UM JOVEM ADULTO')
+
+//}else{
+    //document.write('VELHO')
+//}
+
+let idade= prompt('Digite sua idade')
+if(idade < 18){
+    document.write('Não pode entrar')
+}else{
+    document.write('Pode entrar')
+}
